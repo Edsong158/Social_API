9175ad1fe357f6a097392e0743caf3e4b8a0e5b0
Edsong158 <150963218+Edsong158@users.noreply.github.com>
2024-02-16 00:12:14 -0500

    User.js complete

---
+++ models/User.js
@@ -0,0 +1,66 @@
+const { model, Schema } = require('mongoose');
+const { hash, compare } = require('bcrypt');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            require: [true, 'You must enter a Username'],
+            unique: true,
+            trimmed: true
+        },
+
+        email: {
+            type: String,
+            required: [true, 'Your must enter an email address'],
+            unique: true,
+            validate: {
+                validator(val) {
+                    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ig.test(val);
+                },
+                message: 'Your need to enter a valid email'
+            }
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    }
+);
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        this.password = await hash(this.password, 10);
+    }
+
+    next();
+});
+
+userSchema.methods.validatePass = async function 
+(formPassword) {
+    const is_valid = await compare(formPassword, this.password);
+
+    return is_valid;
+}
+
+userSchema.set('toJSON', {
+    transform: (_, user) => {
+        delete user.password;
+        delete user.__v;
+        return user;
+    },
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3333;
+
+const connection = require('./config/connection');
+
+const { user_routes } = require('./routes/api'); 
+// Middleware
+app.use(express.json());
+
+// Load Routes 
+app.use('/api', [
+    user_routes
+]);
+
+connection.on('open', () => {
+  app.listen(PORT, () => console.log('Server started on port', PORT));
+});
